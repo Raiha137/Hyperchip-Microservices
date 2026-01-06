1b6e65d9127c99b355edc4337fcf359709031dfa
Fathima Raiha <fraiha137@gmail.com>
2026-01-06 13:08:42 +0400

    Revert "Fix admin dashboard routing and order service client"
    This reverts commit 4da14f1e95a40341ddf7b149a08eb1bac3e5c650.

--- HyperchipAdmin/src/main/java/com/hyperchip/admin/client/SalesReportClient.java
+++ HyperchipAdmin/src/main/java/com/hyperchip/admin/client/SalesReportClient.java
@@ -180,10 +180,10 @@ public class SalesReportClient {
      */
     public DashboardResponseDto getDashboard(String periodType) {
         String url = UriComponentsBuilder
-                .fromHttpUrl(orderServiceBaseUrl + "/api/admin/dashboard")
+                .fromHttpUrl(orderServiceBaseUrl)
+                .path("/api/admin/dashboard")
                 .queryParam("periodType", periodType)
                 .toUriString();
-
 
         return restTemplate.getForObject(url, DashboardResponseDto.class);
     }
