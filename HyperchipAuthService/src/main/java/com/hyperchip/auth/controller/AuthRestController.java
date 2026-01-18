392190577911981ac10c9d7b5f2fd870eeeb708e
Fathima Raiha <fraiha137@gmail.com>
2026-01-18 20:29:36 +0400

    Add AuthRestController test endpoint

---
+++ HyperchipAuthService/src/main/java/com/hyperchip/auth/controller/AuthRestController.java
@@ -0,0 +1,15 @@
+package com.hyperchip.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthRestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Auth service is working";
+    }
+}
