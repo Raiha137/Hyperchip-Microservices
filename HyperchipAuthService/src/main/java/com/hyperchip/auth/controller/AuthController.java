682cffcdf13f308c998ad8b5bd542445a8b3b691
Fathima Raiha <fraiha137@gmail.com>
2026-02-02 15:09:29 +0530

    updated auth

--- HyperchipAuthService/src/main/java/com/hyperchip/auth/controller/AuthController.java
+++ HyperchipAuthService/src/main/java/com/hyperchip/auth/controller/AuthController.java
@@ -33,7 +33,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final UserService userService;
