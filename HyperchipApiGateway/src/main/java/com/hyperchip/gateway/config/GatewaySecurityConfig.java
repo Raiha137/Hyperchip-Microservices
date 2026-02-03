03c99173128f3a74f009381913f9a0af8b42e02c
Fathima Raiha <fraiha137@gmail.com>
2026-02-03 09:03:36 +0530

    create config

--- HyperchipApiGateway/src/main/java/com/hyperchip/gateway/config/GatewaySecurityConfig.java
+++ HyperchipApiGateway/src/main/java/com/hyperchip/gateway/config/GatewaySecurityConfig.java
@@ -4,17 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @Configuration
+@EnableWebFluxSecurity
 public class GatewaySecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange.anyExchange().permitAll())
-                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
-                .build();
+        http
+                .csrf().disable()
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(
+                                "/login",
+                                "/signup",
+                                "/verify-otp",
+                                "/forgot-password",
+                                "/reset-password",
+                                "/oauth2/**",
+                                "/css/**", "/js/**", "/img/**", "/uploads/**",
+                                "/actuator/**"
+                        ).permitAll()
+                        .anyExchange().permitAll() // or adjust to your needs
+                )
+                .formLogin().disable()
+                .httpBasic().disable();
+        return http.build();
     }
 }
